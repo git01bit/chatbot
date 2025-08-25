4d2d7d95b8893295afb02166678ad24877750ac4
git01bit <majidpour018@gmail.com>
2025-08-25 14:33:14 +0330

    feat(user-message): read user input and display it as user message

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,64 @@ import { fab } from "@fortawesome/free-brands-svg-icons";
 library.add(fas, far, fab);
 
 import "./assets/fonts/vazir/vazir.css";
+import { useState } from "react";
 
 function App() {
+  const [userMessage, setUserMessage] = useState({
+    id: "",
+    message: "",
+  });
+  const [messages, setMessages] = useState([]);
+
+  function fetchUserMessage(e) {
+    setUserMessage({
+      id: "user",
+      message: e.target.value,
+    });
+  }
+
+  function addUserMessages() {
+    setMessages((m) => [...m, userMessage]);
+    setUserMessage({
+      id: "",
+      message: "",
+    });
+  }
+
   return (
     <section className="mx-auto flex h-dvh max-w-3xl flex-col justify-between overflow-auto p-4 text-justify">
       <div className="flex flex-col gap-y-8 overflow-auto">
-        <p className="ml-24 w-fit rounded-full bg-gray-100 px-8 py-2">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat,
-          dignissimos.
-        </p>
-        <p className="mr-24 self-end">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure,
-          exercitationem. Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Consequatur voluptatem a, alias libero consequuntur quam laborum
-          excepturi est sapiente nobis! Lorem ipsum dolor sit amet.
-        </p>
+        {messages.map((message, index) =>
+          message.id === "user" ? (
+            <p
+              key={index}
+              className="ml-24 w-fit rounded-full bg-gray-100 px-8 py-2"
+            >
+              {message.message}
+            </p>
+          ) : (
+            <p className="mr-24 self-end">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure,
+              exercitationem. Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Consequatur voluptatem a, alias libero consequuntur quam
+              laborum excepturi est sapiente nobis! Lorem ipsum dolor sit amet.
+            </p>
+          ),
+        )}
       </div>
 
       <div className="relative mt-4 rounded-2xl border border-gray-200 px-2 py-1">
         <textarea
           placeholder="سوال خود را مطرح کنید ..."
           className="field-sizing-content max-h-80 min-h-32 w-full resize-none p-6 text-justify outline-none"
+          value={userMessage.message}
+          onChange={fetchUserMessage}
         ></textarea>
 
         <button
           type="button"
           className="absolute bottom-6 left-6 h-8 w-8 cursor-pointer rounded-full bg-gray-200 text-white transition-colors duration-300 ease-in-out hover:bg-black"
+          onClick={addUserMessages}
         >
           <FontAwesomeIcon icon="fa-solid fa-arrow-up" />
         </button>
