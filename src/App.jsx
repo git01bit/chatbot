931d3fc6d04ecbeaa5a4abf0d00819e16503faf2
git01bit <majidpour018@gmail.com>
2025-08-25 10:44:33 +0330

    chore(project): set up project environment

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+library.add(fas, far, fab);
+
+import "./assets/fonts/vazir/vazir.css";
+
+function App() {}
+
+export default App;
